0ebea7444593100c6644ea37cc472c0a10a19e72
Witty-dot <georgelucas78.uk@gmail.com>
2025-11-20 01:06:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { createDeck, dealCards } = require('./poker');
 const path = require('path');
 const app = express();
 app.use(cors());
-app.express.json());
+app.use(express.json());
 // Отдаём фронтенд
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
